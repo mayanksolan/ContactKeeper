16d674bd27fac1f3ab19ba6d00ef4c9a87fc71fa
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-07-13 23:45:47 +0530

    get contacts

--- routes/auth.js
+++ routes/auth.js
@@ -2,16 +2,24 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const { check, validationResult } = require("express-validator");
+const auth = require("../middleware/auth");
 const config = require("config");
+const { check, validationResult } = require("express-validator");
 
 const User = require("../models/User");
 
 // @route       GET api/auth
 // @desc        Get logged in User
 // @access      Private
-router.get("/", (req, res) => {
-  res.send("Get logged in User");
+router.get("/", auth, async (req, res) => {
+  try {
+    console.log(req.user);
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 // @route       POST api/auth
