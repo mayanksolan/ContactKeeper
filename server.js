7552bccefd859cedcd6166a739e6431664868cf1
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-07-17 13:08:03 +0530

    publish

--- server.js
+++ server.js
@@ -10,11 +10,6 @@ connectDB();
 //Init middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => {
-  //res.send("Contact Keeper Hello");
-  res.json({ msg: "Welcome to Contact Keeper API..." });
-});
-
 // Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
